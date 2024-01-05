9ce15ef0e12c11e5525c81be8d0a381485c9d5a3
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-01-06 03:29:16 +0600

    Initial Commit

---
+++ iProspects/iProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iProspectsApp.swift
+//  iProspects
+//
+//  Created by A.f. Adib on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct iProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
